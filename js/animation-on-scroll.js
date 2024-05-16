69730ca72decb2ba7a6f6151cbf27b2346580c6b
Rcasas14 <reymartcasas123@gmail.com>
2024-05-16 09:41:30 +0800

    polishing the animation

--- js/animation-on-scroll.js
+++ js/animation-on-scroll.js
@@ -9,7 +9,8 @@ const observer = new IntersectionObserver((entries) => {
         if(entry.isIntersecting){
             entry.target.classList.add('show');
         } else {
-            entry.target.classList.remove('show');
+            // entry.target.classList.remove('show');
+            console.log(entry);
         }
     })
 })
